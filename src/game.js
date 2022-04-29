c7011640cb2dc2a0a1bbc3e43cc81138fd15a2b5
PhazonicRidley <ma13hew@gmail.com>
2022-04-29 11:51:00 -0700

    added comments for a creation grid, changed height and width to constants

--- src/game.js
+++ src/game.js
@@ -6,14 +6,23 @@ let tictactoeboard =
     ['','',''],
     ['','','']
 ];
+let MAX_HEIGHT = 400;
+let MAX_WIDTH = 400;
 
 
-
-
+function createGrid()
+{
+    // use line() to draw
+    // use relationships 
+    // divide height by 3 to place lines
+    // divide width by 3 to place lines
+    // divide each of those positions by 2 (1/6 of the height/width) to get the center of box
+    
+}
 
 function setup()
 {
-    createCanvas(400, 400);
+    createCanvas(MAX_HEIGHT, MAX_WIDTH);
 }
 
 function draw()
